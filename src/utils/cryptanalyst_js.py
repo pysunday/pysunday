e3639eedc5a5c2ff8eecad575c35164cce1e835c
rnet <chinaitlearner@gmail.com>
2024-11-04 00:47:19 +0800

    fix: add cryptByCrypto api

--- src/utils/cryptanalyst_js.py
+++ src/utils/cryptanalyst_js.py
@@ -15,15 +15,34 @@ function decrypt(datastrList, publicKey) {
 }
 """ % (execPath, ))
 
+crypto = execjs.compile("""
+const crypto = require('%s/crypto.js')
+
+function encrypt(datastrList) {
+    return datastrList.map(each => crypto.encrypt(each))
+}
+""" % (execPath, ))
+
 def cryptBySm4Js(datastrList, key, cryptType='encrypt'):
     if not datastrList: return []
     if type(datastrList) == str:
         datastrList = [datastrList]
     return sm4.call(cryptType, datastrList, key)
 
+def cryptByCrypto(datastrList, cryptType='encrypt'):
+    # 探迹用的加密
+    if not datastrList: return []
+    if type(datastrList) == str:
+        datastrList = [datastrList]
+    return crypto.call(cryptType, datastrList)
+
 if __name__ == "__main__":
     publicKey = '596b71524573654d366b627244346b65'
     origin = '{"a":"haha"}'
+
     target = cryptBySm4Js(origin, publicKey, 'encrypt')
     print(f"========》cryptBySm4('{origin}', '{publicKey}', 'decrypt'): {target}")
     print(f"========》cryptBySm4('{target}', '{publicKey}', 'decrypt'): {cryptBySm4Js(target, publicKey, 'decrypt')}")
+
+    target = cryptByCrypto(origin, 'encrypt')
+    print(f"========》cryptByCrypto('{origin}', 'encrypt'): {target}")
